fc09da3a95e72fc8f5f203c0b80b51a1d2d22575
Alice Kapranchikova <kapranchikovaal@gmail.com>
2020-10-08 13:27:53 +0300

    change servis

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,10 +1,34 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {map} from 'rxjs/operators';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+  }
+
+
+  getAll(): Observable<any> {
+    return this.http.get('').pipe(map(res => res));
+  }
+
+  get() {
+    return this.http.get('').pipe(map(res => res));
+  }
+
+  add() {
+    return this.http.post('', {}).pipe(map(res => res));
+  }
+
+  update() {
+    return this.http.put('', {}).pipe(map(res => res));
+  }
+
+  delete() {
+    return this.http.delete('').pipe(map(res => res));
+  }
 }
